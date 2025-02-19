3a133bb7d231088f13c5e14f839c5623eee5c2f8
Abdalazizi <a.twariki@alustudent.com>
2025-02-19 04:05:44 -0800

    Final Assignment Submission

--- submission_reminder_Twariq/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Resolve script directory and execute reminder
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "Starting the Submission Reminder App..."
-bash "$SCRIPT_DIR/app/reminder.sh"
